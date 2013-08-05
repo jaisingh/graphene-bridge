da77e2baf00a4908fe9678b6cd5d4d379d00b516
Jai Singh <jaisingh@gmail.com>
2013-08-04 21:03:39 -0400

    set pruning, db cleanup, trap break

--- loader.rb
+++ loader.rb
@@ -3,6 +3,12 @@ require 'redis'
 require 'json'
 require 'pp'
 
+trap("SIGINT") do
+  exit 0
+end
+
+SPEED_LOAD = false
+
 ENV['REDIS_URL'] = 'redis://redistogo:130e70de63de375e7fe12fe6654a4e3c@grouper.redistogo.com:9791/'
 
 db = Redis.new
@@ -11,16 +17,20 @@ key = "jai"
 series = %w|sa sb sc|
 entries = 200
 
-db.del key
-
+deleted = db.zremrangebyscore(key,0,Time.now.to_i - 3600)
+puts "#{deleted} records removed"
+sleep 3
 i = 0
 x = 0
+time_spread = SPEED_LOAD ? 300 : 1
+
 while x < entries
   data = [series[rand(3)],rand(120)]
-  now = Time.now.to_i - rand(300)
+  now = Time.now.to_i - time_spread
   puts "Adding #{now} : #{data.to_json}"
   db.zadd(key,now,data.to_json)
-  i += 1  
+  i += 1
+  sleep 1 unless SPEED_LOAD  
 end
 
 sleep 120
